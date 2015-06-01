b9ae4e362bd07b014c598ba72789b2f74996f025
Art Gillespie <art@nestlabs.com>
2015-06-01 07:52:56 -0700

    Initial work on project.

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# 
+#
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
@@ -13,25 +13,46 @@ def connect():
 
 def deleteMatches():
     """Remove all the match records from the database."""
+    conn = connect()
+    cur = conn.cursor()
+    cur.execute('DELETE FROM match')
+    conn.commit()
+    conn.close()
 
 
 def deletePlayers():
     """Remove all the player records from the database."""
+    conn = connect()
+    cur = conn.cursor()
+    cur.execute('DELETE FROM player')
+    conn.commit()
+    conn.close()
 
 
 def countPlayers():
     """Returns the number of players currently registered."""
+    conn = connect()
+    cur = conn.cursor()
+    cur.execute('SELECT COUNT(*) FROM player')
+    count = cur.fetchone()
+    conn.close()
+    return count[0]
 
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
-  
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
+    conn = connect()
+    cur = conn.cursor()
+    cur.execute('INSERT INTO player (name) VALUES (%s)', (name,))
+    conn.commit()
+    conn.close()
 
 
 def playerStandings():
@@ -47,6 +68,22 @@ def playerStandings():
         wins: the number of matches the player has won
         matches: the number of matches the player has played
     """
+    sql = """SELECT player.id, player.name, wins.count, COUNT(match.winner_id)
+                 FROM player
+                 LEFT JOIN match
+                 ON match.loser_id=player.id
+                 OR match.winner_id=player.id
+                 LEFT JOIN wins
+                 ON wins.id=player.id
+                 GROUP BY player.id, player.name, wins.count
+                 ORDER BY wins.count DESC
+                 """
+    conn = connect()
+    cur = conn.cursor()
+    cur.execute(sql)
+    r = cur.fetchall()
+    conn.close()
+    return r
 
 
 def reportMatch(winner, loser):
@@ -56,16 +93,40 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
- 
- 
+    sql = """INSERT INTO match (loser_id, winner_id)
+             VALUES (%s, %s)"""
+    conn = connect()
+    cur = conn.cursor()
+    cur.execute(sql, (loser, winner))
+    conn.commit()
+
+
+def previousMatch(a, b):
+    """Checks to see if these two players have already had a match.
+
+    Args:
+        a: the id number of the first player to check.
+        b: the id number of the second player to check.
+    """
+    sql = """SELECT COUNT(*) FROM match
+             WHERE (loser_id = %s AND winner_id = %s)
+             OR (loser_id = %s AND winner_id = %s)"""
+    conn = connect()
+    cur = conn.cursor()
+    cur.execute(sql, (a, b, b, a))
+    r = cur.fetchone()[0]
+    conn.close()
+    return 1 == r
+
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
@@ -73,5 +134,13 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
+    # since playerStandings() comes back sorted by wins, we can just pair
+    # neighbors in this list
+    # TODO: ensure players haven't already played each other
+    standings = playerStandings()
+    ret = []
+    for i in range(0, len(standings), 2):
+        ret.append((standings[i][0], standings[i][1],
+                    standings[i+1][0], standings[i+1][1]))
 
-
+    return ret
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,26 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+DROP TABLE IF EXISTS player CASCADE;
+CREATE TABLE player (
+	id serial PRIMARY KEY,
+	name text 
+);
+
+DROP TABLE IF EXISTS match CASCADE;
+CREATE TABLE match (
+	loser_id int references player (id),
+	winner_id int references player (id)
+);
+
+DROP VIEW IF EXISTS wins CASCADE;
+CREATE VIEW wins AS SELECT player.id AS id, COUNT(match.winner_id) AS count
+    FROM player
+    LEFT JOIN match
+    ON player.id=match.winner_id
+    GROUP BY player.id;
+
+	
+	
+
 
